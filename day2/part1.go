28e3670d67fa460ddecb069c282288d667ba5462
paszymaja <sprzepiora@hotmail.co.uk>
2021-12-11 14:10:37 +0100

    day2

--- day2/part1.go
+++ day2/part1.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+type mv struct {
+	dir string
+	val int
+}
+
+func main() {
+	path := "day2/input.txt"
+	var mvs []mv
+	inFile, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err.Error() + `: ` + path)
+		return
+	}
+	defer inFile.Close()
+
+	scanner := bufio.NewScanner(inFile)
+	for scanner.Scan() {
+		split := strings.Split(scanner.Text(), " ")
+		v, err := strconv.Atoi(split[1])
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		mvs = append(mvs, mv{split[0], v})
+	}
+
+	var h, d, aim int
+
+	for _, i := range mvs {
+		switch i.dir {
+		case "forward":
+			h += i.val
+			d += (aim * i.val) //only thing added for part2
+		case "down":
+			d += i.val
+		case "up":
+			d -= i.val
+		}
+	}
+	fmt.Print(h * d)
 }
